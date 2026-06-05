5714d7a047daecdd62b2fd4bb64a5be5a64f8a5f
Zemichael Sishah <zemichaelsishah1@gmail.com>
2026-06-05 13:29:55 -0400

    limmited sized stack (needs debugging)

--- stacks.c
+++ stacks.c
@@ -1,9 +1,65 @@
 #include <stdio.h>
-#define SIZE sizeof(arr)/sizeof(arr[0])
+#define SIZE 10
 
+
+void push(int *top, int *status, int num){
+    if(*status != 0){
+        *top++;
+    }
+    (*status)++;
+    *top = num;
+}
+
+int pop(int *top, int *status){
+    int temp = *top;
+    *top = 0;
+    *top--;
+    (*status)--;
+    printf("number popped %d\n", temp);
+    return temp;
+
+}
+
+int check(int status){
+    if(status >= SIZE || status < 0){
+        return 1;
+    }
+    return 0;
+}
 
 int main(void){
-    int arr[10];
-    printf("size of array is %d\n", SIZE);
+    int status = 0;
+    int arr[SIZE];
+    char x = 'c';
+    int *up = &arr[0];
+    printf("hjfsfvbljhvafjlh  %d\n", arr[8]);
+    do{
+        printf("choose an operation:\na)pop\nb)push\nc)done\n");
+        scanf(" %c", &x);
+        if(check(status)){
+            break;
+        }
+
+        switch(x){
+            case 'a':
+                if(status != 0){
+                    pop(up, &status);
+                }
+                break;
+            case 'b':
+                char num;
+                printf("number to add");
+                scanf("%d", &num);
+                push(up, &status, num);
+                break;
+            default:
+                continue;
+                
+        }
+
+    }while(x != 'c');
+
+    printf("terminated");
+
     return 0;
 }
